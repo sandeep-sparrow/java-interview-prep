7cbf37ff560deee7805efc703c43666a5be55bff
sandeep-simplify <sandeep@simplifymoney.in>
2024-04-15 22:04:40 +0530

    Liner Queue DS using Array's

---
+++ src/main/java/faang/dsa/queue/MainQueueArray.java
@@ -0,0 +1,26 @@
+package faang.dsa.queue;
+
+public class MainQueueArray {
+
+    public static void main(String[] args) {
+
+        QueueArray myQueueArray = new QueueArray(3);
+        System.out.println("Queue is Empty: " + myQueueArray.isFull());
+        System.out.println("Queue is Empty: " + myQueueArray.isEmpty());
+
+        myQueueArray.enqueue(10);
+        myQueueArray.enqueue(11);
+        myQueueArray.enqueue(12);
+        myQueueArray.enqueue(13);
+
+        System.out.println("Result of 1st dequeue: " + myQueueArray.dequeue());
+        System.out.println("Result of peek: " + myQueueArray.peek());
+        System.out.println("Result of 2nd dequeue: " + myQueueArray.dequeue());
+        System.out.println("Result of 3rd dequeue: " + myQueueArray.dequeue());
+        System.out.println("Result of dequeue: " + myQueueArray.dequeue());
+        System.out.println("Result of peek: " + myQueueArray.peek());
+
+        myQueueArray.deleteQueue();
+
+    }
+}
